7ce68f21d2302d12e1f0b842de33fa6bdf9d5a72
Rohit Rajpurohit <rajpurohit9673@gmail.com>
2025-01-31 23:32:42 +0530

    Added CH4

---
+++ Chapter_04/01_List.py
@@ -0,0 +1,13 @@
+# List can contain differet data type as we can see below
+listex = ["Rohit","Mohit",False,334,'R']
+
+# List can be Change but String can't Change as expriment below
+print(listex[0])#Printed the index 0 value
+listex[0] = "RR"#changed the index 0's value
+print(listex[0])#printed here to confirm 
+
+# To print one index to another
+print(listex[0:4])
+# To print all
+print(listex)
+
