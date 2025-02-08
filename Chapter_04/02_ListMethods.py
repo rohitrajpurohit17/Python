c9846a8ee786d00336bd3fa5f7d4bfa44844655b
Rohit Rajpurohit <rajpurohit9673@gmail.com>
2025-02-08 16:38:43 +0530

    Added 02 in CH4

---
+++ Chapter_04/02_ListMethods.py
@@ -0,0 +1,22 @@
+listex = ["Rohit","Mohit",False,334,'R']
+#append means in english to join at the end
+listex.append("DR")
+print(listex)
+
+num = [20,21,5,1,2,90]
+print(num)
+#Putting the data in order by using function sortting
+num.sort()
+print(num)
+#Reversing the data
+num.reverse()
+print(num)
+#add a particular value on mentioned index
+num.insert(2,2021)
+print(num)
+#Poping data from particular index and also printing the removig value
+print(num.pop(2))
+print(num)
+#Deleting data by mentioning value
+num.remove(1)
+print(num)
