4009bb82c56201a9deeaf0cbc8ea7154c9fde834
Rohit Rajpurohit <rajpurohit9673@gmail.com>
2025-02-09 16:32:08 +0530

    Compeleted CH04

---
+++ Chapter_04_Practice/Problem03.py
@@ -0,0 +1,4 @@
+# Check that a tuple type cannot be changed in python.
+a = (34,2,"Rohit")
+
+a[2] = "Mohit"
---
+++ Chapter_04_Practice/Problem04.py
@@ -0,0 +1,7 @@
+# Write a program to sum a list with 4 numbers.
+
+num = [20, 21, 9]
+r = (20,21,89)
+# Sum is inbuild fucntion which helps in addition
+print(sum(num))
+print(sum(r))
