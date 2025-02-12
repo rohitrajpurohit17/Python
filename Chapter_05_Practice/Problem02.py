f5af73631a7bb92da667fc6013728a332369bff5
Rohit Rajpurohit <rajpurohit9673@gmail.com>
2025-02-12 22:02:23 +0530

    Updated CH05

---
+++ Chapter_05_Practice/Problem02.py
@@ -0,0 +1,19 @@
+# Write a program to input eight numbers from the user and display all the unique 
+# numbers (once). 
+s = set()
+n =  int(input("Enter the Number : "))
+s.add(n)
+n =  int(input("Enter the Number : "))
+s.add(n)
+n =  int(input("Enter the Number : "))
+s.add(n)
+n =  int(input("Enter the Number : "))
+s.add(n)
+n =  int(input("Enter the Number : "))
+s.add(n)
+n =  int(input("Enter the Number : "))
+s.add(n)
+n =  int(input("Enter the Number : "))
+s.add(n)
+
+print(s)
